04a3458276338d849660ff3ac94932ebfd6e2ee3
Zach Vega-Perkins <zachv@wepay.com>
2017-04-14 13:19:14 -0700

    [iOS] Remove internal headers from 6.0.0 docs
    Summary: regenerated docs against [[ https://github.devops.wepay-inc.com/sdk/wepay-ios-priv/commit/b3ce2e862c7b55716c898c6d19df033fd52b79f1 | corresponding release commit ]] with incorrect header files excluded.
    
    Test Plan:
    * After patching, open documentation/html/6.0.0/index.html
    * Look at "Files/File List." There should be 9 headers total and none of them should be internal-only files
    
    Reviewers: #mobile, chaitanya.bagaria
    
    Reviewed By: #mobile, chaitanya.bagaria
    
    Differential Revision: https://phabricator.corp.wepay-inc.com/D11345

--- 6.0.0/files.js
+++ 6.0.0/files.js
@@ -7,9 +7,6 @@ var files =
     [ "WPConfig.h", "_w_p_config_8h_source.html", null ],
     [ "WPError.h", "_w_p_error_8h.html", "_w_p_error_8h" ],
     [ "WPMockConfig.h", "_w_p_mock_config_8h_source.html", null ],
-    [ "WPMockRoamConfigurationManager.h", "_w_p_mock_roam_configuration_manager_8h_source.html", null ],
-    [ "WPMockRoamDeviceManager.h", "_w_p_mock_roam_device_manager_8h_source.html", null ],
-    [ "WPMockRoamTransactionManager.h", "_w_p_mock_roam_transaction_manager_8h_source.html", null ],
     [ "WPPaymentInfo.h", "_w_p_payment_info_8h_source.html", null ],
     [ "WPPaymentToken.h", "_w_p_payment_token_8h_source.html", null ]
 ];
