c6a365608a1accdd2312e96ded95a349c288c200
Cameron Alley <camerona@wepay.com>
2017-04-18 16:21:53 -0700

    added doc for version 7.0.0-prerelease-1

---
+++ 7.0.0-prerelease-1/search/all_5.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['firstname',['firstName',['../interface_w_p_payment_info.html#a7b58337ade7f26daf199e2385e7a8e71',1,'WPPaymentInfo']]],
+  ['forgetrememberedcardreader',['forgetRememberedCardReader',['../interface_we_pay.html#af9a2f0b02a91787b7a03f291e75a7f89',1,'WePay']]]
+];
